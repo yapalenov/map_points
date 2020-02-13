3306865e6160ad219934918c952f512984b1ce5e
palenov <palenov.yaroslav@gmail.com>
2020-02-13 12:25:31 +0300

    Updates

---
+++ precache-manifest.2336ac04530dc9a0756f831a552195f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b42cff53d0372b4932c5dd0f774186",
+    "url": "/map_points/index.html"
+  },
+  {
+    "revision": "d905922e08e9ee3cc902",
+    "url": "/map_points/static/css/main.4825e5ee.chunk.css"
+  },
+  {
+    "revision": "1d5ccf11b5ef1595ac6d",
+    "url": "/map_points/static/js/2.29abcfa7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/map_points/static/js/2.29abcfa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d905922e08e9ee3cc902",
+    "url": "/map_points/static/js/main.b7b3752a.chunk.js"
+  },
+  {
+    "revision": "5ddf3acfe2ee67d3fd80",
+    "url": "/map_points/static/js/runtime-main.60e0e95e.js"
+  }
+]);
